8abb88c5bed9f57883f5b67dc6018906a0e9d5b2
Arthur Azevedo França <arthurfrana@gmail.com>
2025-10-11 16:28:54 -0300

    feat: upgrade version

--- MouseGlob/src/dcc/mouseglob/MouseGlob.java
+++ MouseGlob/src/dcc/mouseglob/MouseGlob.java
@@ -47,7 +47,7 @@ import dcc.tree.TreeManager;
 public class MouseGlob extends
 		AbstractModule<MouseGlobApplet, MouseGlobUI, MouseGlobController> {
 
-	public static final String VERSION = "1.6.7";
+	public static final String VERSION = "2.0.1";
 	public static final String HEADER = String.format("MouseGlob %s - Daniel Coelho de Castro (2009-2016)", VERSION);
 
 	@Inject
